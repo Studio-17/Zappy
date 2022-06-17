fc25466c79f85cf77f6ece159dfd04e6d247c72c
Martin Vanaud <martin.vanaud@epitech.eu>
2022-06-17 21:46:30 +0200

    feat(zappy_server) added header protoco for player positon server-side

--- project/server/source/server/communication/communication.c
+++ project/server/source/server/communication/communication.c
@@ -19,19 +19,19 @@ void handle_client(zappy_t *zappy)
 
         zappy->server->sd->socket_descriptor = zappy->server->ss->client[index];
 
-        request_payload_player_position_t player_position_request = get_request_player_position(zappy->server->sd->socket_descriptor);
+        // request_payload_player_position_t player_position_request = get_request_player_position(zappy->server->sd->socket_descriptor);
 
-        payload_header_t header = {
-            .size = sizeof(payload_header_t),
-            .type = 0,
-        };
-        post_header(zappy->server->sd->socket_descriptor, header);
+        // payload_header_t header = {
+        //     .size = sizeof(payload_header_t),
+        //     .type = 0,
+        // };
+        // post_header(zappy->server->sd->socket_descriptor, header);
 
-        response_payload_player_position_t response = {
-            .status = true,
-            .player_id = zappy->client[player_position_request.player_id].client_nb,
-            .position = zappy->client[player_position_request.player_id].player.position,
-        };
-        post_response_player_position(zappy->server->sd->socket_descriptor, response);
+        // response_payload_player_position_t response = {
+        //     .status = true,
+        //     .player_id = zappy->client[player_position_request.player_id].client_nb,
+        //     .position = zappy->client[player_position_request.player_id].player.position,
+        // };
+        // post_response_player_position(zappy->server->sd->socket_descriptor, response);
     }
 }
