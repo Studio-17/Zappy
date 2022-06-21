c0dc0c7ad9ffdbd763940319148116be04f3a4b5
Gurvan Le Letty <gurvan.le-letty@epitech.eu>
2022-06-21 19:46:42 +0200

    feat(server Look): added Look server request handle to return to client all visible tiles && remove movement in Left & Right request

--- project/server/source/server/communication/ai.c
+++ project/server/source/server/communication/ai.c
@@ -30,6 +30,11 @@ void ai_response_ok_ko(int socket, bool status)
     write(socket, (status ? "ok\n" : "ko\n" ), message_size);
 }
 
+void ai_response_string(int socket, char *message)
+{
+    dprintf(socket, "%s\n", message);
+}
+
 static void move(zappy_t *zappy, position_t movement, int player_index)
 {
     zappy->client[player_index].player.position.x += movement.x;
@@ -57,30 +62,82 @@ void ai_forward_request(zappy_t *zappy, void *data, int player_index)
 void ai_right_request(zappy_t *zappy, void *data, int player_index)
 {
     int socket = zappy->server->socket_descriptor->socket_descriptor;
-    position_t movement;
 
     printf("position of player before: %dx, %dy\n", zappy->client[player_index].player.position.x, zappy->client[player_index].player.position.y);
     zappy->client[player_index].player.orientation += 1;
     if (zappy->client[player_index].player.orientation > WEST)
         zappy->client[player_index].player.orientation = NORTH;
-    movement = direction[zappy->client[player_index].player.orientation];
-    move(zappy, movement, player_index);
     ai_response_ok_ko(socket, true);
 }
 
 void ai_left_request(zappy_t *zappy, void *data, int player_index)
 {
     int socket = zappy->server->socket_descriptor->socket_descriptor;
-    position_t movement;
 
     printf("position of player before: %dx, %dy\n", zappy->client[player_index].player.position.x, zappy->client[player_index].player.position.y);
     if (zappy->client[player_index].player.orientation < NORTH)
         zappy->client[player_index].player.orientation = WEST;
     else
         zappy->client[player_index].player.orientation -= 1;
-    movement = direction[zappy->client[player_index].player.orientation];
-    move(zappy, movement, player_index);
     ai_response_ok_ko(socket, true);
+}
+
+void print_tile_content(int socket, tile_t const tile, bool first)
+{
+    bool first_resource = true;
+
+    for (int index = 0; index < NB_ITEMS; index++)
+        if (tile.resources[index].quantity) {
+            if (!first && first_resource) {
+                dprintf(socket, " ");
+                first_resource = false;
+            } else if (!first_resource)
+                dprintf(socket, " ");
+            dprintf(socket, "%s", tile.resources[index].name);
+        }
+}
+
+void ai_look_request(zappy_t *zappy, void *data, int player_index)
+{
+    int socket = zappy->server->socket_descriptor->socket_descriptor;
+    position_t player_orientation = direction[zappy->client[player_index].player.orientation];
+    position_t position = zappy->client[player_index].player.position;
+    int nb_tiles = 0;
+
+    for (int i = 0; i <= zappy->client[player_index].player.level; i++)
+        nb_tiles += 1 + (i * 2);
+    tile_t *tiles = malloc(sizeof(tile_t) * nb_tiles);
+    if (!tiles)
+        return;
+    int tiles_index = 0;
+    bool first = true;
+    dprintf(socket, "[");
+    for (int i = 0; i <= zappy->client[player_index].player.level; i++) {
+        for (int index = -i; index <= i; index++) {
+            if (!first)
+                dprintf(socket, ",");
+            position_t tmp_pos;
+            tmp_pos.x = zappy->client[player_index].player.position.x + (player_orientation.x * index);
+            if (tmp_pos.x >= zappy->map->width)
+                tmp_pos.x = 0;
+            else if (tmp_pos.x < 0)
+                tmp_pos.x = zappy->map->width - 1;
+                        // tiles[tiles_index].x = zappy->client[player_index].player.position.x + (player_orientation.x * index);
+            tmp_pos.y = zappy->client[player_index].player.position.y + (player_orientation.y * index);
+            if (tmp_pos.y >= zappy->map->height)
+                tmp_pos.y = 0;
+            else if (tmp_pos.y < 0)
+                tmp_pos.y = zappy->map->height - 1;
+
+                        // tiles[tiles_index].y = zappy->client[player_index].player.position.y + (player_orientation.y * index);
+            print_tile_content(socket, zappy->map->tiles[tmp_pos.y][tmp_pos.x], first);
+            tiles[tiles_index] = zappy->map->tiles[tmp_pos.y][tmp_pos.x];
+            tiles_index++;
+            if (first)
+                first = false;
+        }
+    }
+    dprintf(socket, "]\n");
 }
 
 static char *get_resource_from_request(char *target)
@@ -108,7 +165,7 @@ static char *get_resource_from_request(char *target)
 
 void ai_take_request(zappy_t *zappy, void *data, int player_index)
 {
-    char *request = *(char *)data;
+    char *request = (char *)data;
     char *resource = get_resource_from_request(request);
 
     if (resource == NULL)
@@ -121,7 +178,7 @@ void ai_take_request(zappy_t *zappy, void *data, int player_index)
 
 void ai_set_request(zappy_t *zappy, void *data, int player_index)
 {
-    char *request = *(char *)data;
+    char *request = (char *)data;
     char *resource = get_resource_from_request(request);
 
     if (resource == NULL)
@@ -151,7 +208,7 @@ static const ai_request_t ai_request_to_handle[] = {
     {
         .request = "Look\n",
         .command = LOOK,
-        .handler = &ai_base_request
+        .handler = &ai_look_request
     },
     {
         .request = "Inventory\n",
