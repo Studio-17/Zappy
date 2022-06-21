f1316b212c76100317f66d3b459bbc5ec19fdae9
Clement Fernandes <clement.fernandes@epitech.eu>
2022-06-21 13:37:43 +0200

    feat(tile): added a generic tile that collect the informations of the ressources

--- project/client/gui/Source/App/Object/Tile/Tile.hpp
+++ project/client/gui/Source/App/Object/Tile/Tile.hpp
@@ -14,17 +14,23 @@
 namespace Object {
     class Tile : public AThreeDimensionObject {
         public:
-            Tile(Object::Render::MyModel &pathToModel, Object::Render::MyTexture &pathToRessources, Position const &position, Object::MAP_OBJECTS type);
+            Tile(Object::Render::MyModel &pathToModel, Object::Render::MyTexture &pathToRessources,  std::vector<Object::Render::MyModel> resourcesModels, Position const &position, Object::MAP_OBJECTS type);
             ~Tile();
 
             void draw() override;
 
             std::vector<std::shared_ptr<Object::Resource>> getResources() const;
-            void setResources(std::vector<std::shared_ptr<Object::Resource>> const &resources);
+            void setResources(std::vector<std::pair<Object::PLAYER_RESSOURCES, int>> const &resources);
+
+            Position getPosition() const { return _position; }
+            void setPosition(Position const &position) { _position = position; }
 
         protected:
         private:
             std::vector<std::shared_ptr<Object::Resource>> _resources;
+            std::vector<Object::Render::MyModel> _resourcesModels;
+
+            Position _position;
     };
 }
 
